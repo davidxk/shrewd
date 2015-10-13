a4d0884dd5a9e39e299e3ff43c41183e0c0b0cdc
Zhao Tianyu <davidxk@qq.com>
2015-10-13 16:03:11 +0800

    Handler mostly done, except readInquiry

--- source/comm/handler.cpp
+++ source/comm/handler.cpp
@@ -45,7 +45,7 @@ void PlayerShell::readBlind(string& message)
 	sprintf(pid_ch, "%d", pid);
 	psr->matchWColon(msg);
 	bet=psr->nextInt(msg); //now at eol
-	psr->matchChar(msg); //now at ^
+	psr->matchWord(msg); //now at ^
 
 	Action sact;
 	sact.act=act;
@@ -82,12 +82,113 @@ void PlayerShell::readHold(string& message)
 		hole.push_back(psr->getCard(msg));
 
 	player->rcvHole(hole);
+	//consume tail if needed here
 }
 
 void PlayerShell::readInquire(string msg)
 {
+	//A player reads what he expects to see so the handler simply passes on
 
 }
+
+void PlayerShell::readFlop(string& msg)
+{
+	psr->matchHead(msg);
+
+	vector<Card> comm;
+	for(int i=0;i<N_FLOP;i++)
+		comm.push_back(psr->getCard(msg));
+	//caution here, plyrshel judged if msg empty here 
+
+	player->rcvFlop(comm);
+	player->cntRd=0; //optimize here later
+	//consume tail if needed here
+}
+
+void PlayerShell::readTurn(string& msg)
+{
+	psr->matchHead(msg);
+
+	player->rcvTurn( psr->getCard(msg) );
+	//caution here, plyrshel judged if msg empty here 
+	player->cntRd=0; //optimize here later
+}
+
+void PlayerShell::readRiver(string& msg)
+{
+	psr->matchHead(msg);
+
+	player->rcvRiver( psr->getCard(msg) );
+	//caution here, plyrshel judged if msg empty here 
+	player->cntRd=0; //optimize here later
+}
+
+void PlayerShell::readShowdown(string& msg)
+{
+	psr->matchHead(msg);
+	psr->matchHead(msg);
+
+	int pid, nut_hand;
+	char pid_ch[20];
+
+	for(int i=0;i<COMM_SIZE;i++)
+		psr->getCard(msg);
+	psr->matchTail();
+
+	while(msg[0]!='/')
+	{
+		psr->matchWColon(msg);
+
+		pid=psr->nextInt(msg);
+		sprintf(pid_ch, "%d", pid);
+		psr->matchWord(msg);
+
+		for(int i=0;i<2;i++)
+			player->rcvPHole(string(pid_ch), psr->getCard(msg));
+		
+		//deal with nut hand with switch(msg[1]) cont
+		switch(msg[1])
+		{
+			case 'I': nut_hand=HIGHCARD; break;
+			case 'N': nut_hand=PAIR; break;
+			case 'W': nut_hand=TWOPAIRS; break;
+			case 'H': nut_hand=TRIP; break;
+			case 'L': nut_hand=FLUSH; break;
+			case 'U': nut_hand=FULLHOUSE; break;
+			case 'O': nut_hand=FOUR; break;
+			case 'T':
+					  if(msg[8]=='_') nut_hand=STRAIGHTFLUSH;
+					  else nut_hand=STRAIGHT;
+					  break;
+			default: cout<<"Error: Unknow hand. Protocal Unmatch. "<<endl;
+		}
+		player->rcvPHand(string(pid_ch), nut_hand);
+		psr->matchWord(msg);
+	}
+	psr->matchTail(msg);
+}
+
+void PlayerShell::readPotwin(string& msg)
+{
+	string msg=sticky(message, "pot-win/");
+	psr->matchHead(msg);
+
+	int pid, share;
+	char pid_ch[20];
+
+	while(msg[0]!='/')
+	{
+		pid=psr->nextInt(msg);
+		sprintf(pid_ch, "%d", pid);
+		psr->matchWColon(msg);
+		share=psr->nextInt(msg); //now at eol
+		psr->matchWord(msg); //now at ^
+
+		player->rcvPotwin(string(pid_ch), share);
+	}
+	psr->matchTail(msg);
+}
+
 
 
 
