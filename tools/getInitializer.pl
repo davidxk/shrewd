25bc8c5bdbaa91ec4935cdfd0c638de288770ada
Zhao Tianyu <davidxk@qq.com>
2015-10-24 22:03:52 +0800

    Better code gen tools

---
+++ tools/getInitializer.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl 
+
+my $class_name, $arg_list;
+my @member_name, @member_type, @member_dclr;
+while(<>)
+{
+	$class_name=$1 if(/^class (\w*)(:.*)?$/);
+	if(/^\t(\w*) (\w*);/)
+	{
+		#push @member_type, $1;
+		push @member_name, $2;
+
+		my ( $copy_type, $copy_arg ) = ( $1, $2 );
+		$copy_arg=~ s/^(\w)(\w*)$/a\u\1\2/;
+		push @member_dclr, $copy_type.' '.($copy_arg);
+	}
+}
+#print "#include \"$class_name.h\"\n\n";
+print $class_name, "::$class_name(";
+print join ', ', @member_dclr;
+print "):\n";
+print "\t";
+map{ s/^(\w)(\w*)$/\1\2(a\u\1\2)/ } @member_name;
+print join ', ', @member_name;
+print " { }\n"
