cac908e43c314a50b1dfb82363c6ce8fd3ccb23c
Zhao Tianyu <davidxk@qq.com>
2015-11-05 22:37:34 +0800

    Fixed inBet calc bug: RdState constructor init

--- source/model/rdstate.cpp
+++ source/model/rdstate.cpp
@@ -3,7 +3,7 @@
 
 RdState::RdState(): inBet(0) { }
 
-RdState::RdState(PlayerInfo aPi): pi(aPi) { }
+RdState::RdState(PlayerInfo aPi): pi(aPi), inBet(0) { }
 
 RdState::RdState(PlayerInfo aPi, Action aLstAct, int aInBet):
 	pi(aPi), lstAct(aLstAct), inBet(aInBet) { }
